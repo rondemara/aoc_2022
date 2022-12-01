61834bf563744a9db9e31c3d18ff505ac0e7b07b
Ron DeMara <rdemara@aphysci.com>
2022-12-01 16:24:53 -0500

    Completed day 1

--- aoc_day1/src/main.rs
+++ aoc_day1/src/main.rs
@@ -1,3 +1,33 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn calculate_maximum(file: &str) {
+    let mut v : Vec<i32> = Vec::new();
+    let mut accum = 0;
+
+    let ifile = File::open(file).unwrap();
+    let ifile = BufReader::new(ifile);
+
+    for line in ifile.lines() {
+        let s = line.unwrap();
+        if s.is_empty() {
+            v.push(accum);
+            accum = 0;
+        } else {
+            accum += s.parse::<i32>().unwrap();
+        }
+    }
+
+    // Reverse sort the array
+    v.sort_by(|a,b| b.cmp(a));
+
+    // Slice out and sum the top 3
+    let slice : i32 = v[0..3].into_iter().sum();
+    println!("Max: {}, Sum of Top 3: {}", *v.iter().nth(0).unwrap(), slice);
+
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    calculate_maximum("/home/demarr/Projects/aoc_2022/aoc_day1/res/input.txt");
 }
